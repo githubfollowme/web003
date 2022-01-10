1f839c4aa1f0e78da01ab9cf6a7d6f79288e8a43
githubfollowme <flymayday112@gmail.com>
2022-01-10 15:31:50 +0800

    製作後台管理登入功能

--- back/main.php
+++ back/main.php
@@ -1,13 +1,50 @@
+<?php 
+session_start();
+if(!empty($_POST)){
+    if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+        $_SESSION['login']='admin';
+    }else{
+        echo "<div class='ct' style='color:red'>帳號或密碼錯誤</div>";
+    }
+}
 
-<!-- 就是有做表單動作的時候 --> 
-<!-- 原本的isset應改成empty 因為系統預設 所以一定會存在 -->
+?>
+
+<?php
+if(isset($_SESSION['login'])){
+
+?>
 <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
-              <a href="?do=admin&redo=tit">網站標題管理</a>| 
-              <a href="?do=admin&redo=go">動態文字管理</a>| 
-              <a href="?do=admin&redo=rr">預告片海報管理</a>| 
-              <a href="?do=admin&redo=vv">院線片管理</a>| 
-              <a href="?do=admin&redo=order">電影訂票管理</a> 
-            </div>
-            <div class="rb tab">
-                <h2 class="ct">請選擇所需功能</h2>
-            </div>
+    <a href="?do=admin&redo=tit">網站標題管理</a>|
+    <a href="?do=admin&redo=go">動態文字管理</a>|
+    <a href="?do=admin&redo=rr">預告片海報管理</a>|
+    <a href="?do=admin&redo=vv">院線片管理</a>|
+    <a href="?do=admin&redo=order">電影訂票管理</a>
+</div>
+<div class="rb tab">
+    <h2 class="ct">請選擇所需功能</h2>
+</div>
+<?php
+}else{
+?>
+<form action="back.php" method="post">
+    <table class="tab">
+        <tr>
+            <td>帳號:</td>
+            <td><input type="text" name="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼:</td>
+            <td><input type="password" name="pw"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="登入"></td>
+            <td></td>
+        </tr>
+    </table>
+</form>
+
+
+<?php
+}
+?>
