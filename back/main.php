15d8d7064745d83e48054dfc0dbd567d4555b132
githubfollowme <flymayday112@gmail.com>
2022-01-10 15:17:54 +0800

    將前後端主頁進行切版

---
+++ back/main.php
@@ -0,0 +1,13 @@
+
+<!-- 就是有做表單動作的時候 --> 
+<!-- 原本的isset應改成empty 因為系統預設 所以一定會存在 -->
+<div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+              <a href="?do=admin&redo=tit">網站標題管理</a>| 
+              <a href="?do=admin&redo=go">動態文字管理</a>| 
+              <a href="?do=admin&redo=rr">預告片海報管理</a>| 
+              <a href="?do=admin&redo=vv">院線片管理</a>| 
+              <a href="?do=admin&redo=order">電影訂票管理</a> 
+            </div>
+            <div class="rb tab">
+                <h2 class="ct">請選擇所需功能</h2>
+            </div>
