d05f117f69afe3b5c3e7d6706752501dd7eb6b98
githubfollowme <flymayday112@gmail.com>
2022-01-10 16:39:50 +0800

    完成新增預告片功能

---
+++ web003.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-01-10 08:50:57
+-- 伺服器版本： 10.4.21-MariaDB
+-- PHP 版本： 7.4.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `web003`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `movie`
+--
+
+CREATE TABLE `movie` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `level` tinyint(1) NOT NULL,
+  `length` int(5) NOT NULL,
+  `ondate` date NOT NULL,
+  `publish` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `director` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `trailer` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `poster` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `intro` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `rank` int(5) UNSIGNED NOT NULL,
+  `sh` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `ord`
+--
+
+CREATE TABLE `ord` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `no` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `movie` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `date` date NOT NULL,
+  `session` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `qt` int(1) NOT NULL,
+  `seat` text COLLATE utf8mb4_unicode_520_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `poster`
+--
+
+CREATE TABLE `poster` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `path` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `rank` int(5) NOT NULL,
+  `sh` int(1) NOT NULL,
+  `ani` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `ord`
+--
+ALTER TABLE `ord`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `poster`
+--
+ALTER TABLE `poster`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `ord`
+--
+ALTER TABLE `ord`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `poster`
+--
+ALTER TABLE `poster`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
