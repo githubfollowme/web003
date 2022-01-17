e86ddc3b4ad4bc345a6fff96f651006b58d6d124
githubfollowme <flymayday112@gmail.com>
2022-01-17 15:25:05 +0800

    完成院線片清單及分級功能

--- base.php
+++ base.php
@@ -1,14 +1,14 @@
 <?php
 date_default_timezone_set("Asia/Taipei");
 session_start();
-
+//$ls=[1=>'普遍級',2=>"保護級",3=>"輔導級",4=>"限制級"];
 class DB{
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=web003";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=web03";
     protected $user="root";
     protected $pw='';
     protected $pdo;
     protected $table;
-
+    protected $level=[1=>'普遍級',2=>"保護級",3=>"輔導級",4=>"限制級"];
 
     public function __construct($table){
         $this->table=$table;
@@ -125,6 +125,10 @@ class DB{
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    //回傳分級文字
+    public function level($level){
+        return $this->level[$level];
+    }
 
 }
 
