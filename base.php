3b739c04a65ec885fbea70cebe19d3a6ec0df785
keep <flymayday112@gmail.com>
2022-01-21 22:10:29 +0800

    correct the error of base

--- base.php
+++ base.php
@@ -1,87 +1,155 @@
-<?php include_once "../base.php";
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+$ss=[1=>'14:00~16:00',
+     2=>'16:00~18:00',
+     3=>'18:00~20:00',
+     4=>'20:00~22:00',
+     5=>'22:00~24:00',
+];
+//$ls=[1=>'普遍級',2=>"保護級",3=>"輔導級",4=>"限制級"];
+class DB{
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=web003";
+    protected $user="root";
+    protected $pw='';
+    protected $pdo;
+    protected $table;
+    protected $level=[1=>'普遍級',2=>"保護級",3=>"輔導級",4=>"限制級"];
 
-$movie=$Movie->find($_POST['id']);
-$date=$_POST['date'];
-$session=$ss[$_POST['session']];
-sort($_POST['seats']);
-$seats=$_POST['seats'];
-$id=$Ord->math("max","id")+1;
-$no=date("Ymd") . sprintf("%04d",$id);
-
-$Ord->save([
-    'no'=>$no,
-    'movie'=>$movie['name'],
-    'date'=>$date,
-    'session'=>$session,
-    'seat'=>serialize($seats),
-    'qt'=>count($seats)
-]);
-
-?>
-<style>
-    #order{
-        width:60%;
-        margin:auto;
-    }
-    .row{
-        display:flex;
-        width:100%;
-    }
-    .row .first{
-        width:20%;
-        text-align:right;
-    }
-    .row .sec{
-        width:85%;
-        text-align:left;
-    }
-    .sec select{
-        width:100%;
+    public function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,$this->user,$this->pw);
     }
 
-</style>
-<div id="order">
-<div class="row">
-    <div class="sec">
-        感謝您的訂購，您的訂單編號是：<?=$no;?>
-    </div>
-    
-</div>
-<div class="row">
-    <div class="first">電影名稱：</div>
-    <div class="sec">
-        <?=$movie['name'];?>
-    </div>
-</div>
-<div class="row">
-    <div class="first">日期：</div>
-    <div class="sec">
-        <?=$date;?>
-    </div>
-</div>
-<div class="row">
-    <div class="first">場次時間：</div>
-    <div class="sec">
-        <?=$session;?>
-    </div>
-</div>
-<div class="row">
-    <div class="sec">
-        座位:<br>
-        <?php
-        foreach($seats as $seat){
-           echo  (floor($seat/5)+1). "排".($seat%5 +1)."號";
-           echo "<br>";
+    public function find($id){
+        $sql="SELECT * FROM $this->table WHERE ";
+
+        if(is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+
+            $sql .= implode(" AND ",$tmp);
+        }else{
+            $sql .= " `id`='$id'";
         }
 
-        ?>
-        共<?=count($seats);?>張電影票
-    </div>
-</div>
-<div class="row">
-    <div class="ct" style="width:100%">
-        <button onclick="location.href='index.php'">確認</button>
-    </div>
-    
-</div>
-</div>
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    public function all(...$arg){
+        $sql="SELECT * FROM $this->table ";
+
+        switch(count($arg)){
+            case 2:
+                foreach($arg[0] as $key => $value){
+                    $tmp[]="`$key`='$value'";
+                }
+
+                $sql .=" WHERE ".implode(" AND ",$tmp)." ".$arg[1];
+
+            break;
+            case 1:
+                if(is_array($arg[0])){
+                    
+                    foreach($arg[0] as $key => $value){
+                        $tmp[]="`$key`='$value'";
+                    }
+                    $sql .= " WHERE ".implode(" AND ",$tmp);
+                }else{
+                    $sql .= $arg[0];
+                    
+                }
+            break;
+        }
+
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function math($method,$col,...$arg){
+        $sql="SELECT $method($col) FROM $this->table ";
+
+        switch(count($arg)){
+            case 2:
+                foreach($arg[0] as $key => $value){
+                    $tmp[]="`$key`='$value'";
+                }
+
+                $sql .=" WHERE ".implode(" AND ",$tmp)." ".$arg[1];
+
+            break;
+            case 1:
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key => $value){
+                        $tmp[]="`$key`='$value'";
+                    }
+                    $sql .= " WHERE ".implode(" AND ",$tmp);
+                }else{
+                    $sql .= $arg[0];
+                    
+                }
+            break;
+        }
+
+
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    public function save($array){
+        if(isset($array['id'])){
+            //update
+            foreach($array as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql="UPDATE $this->table 
+                     SET ".implode(",",$tmp)."
+                   WHERE `id`='{$array['id']}'";
+        }else{
+            //insert
+
+            $sql="INSERT INTO $this->table (`".implode("`,`",array_keys($array))."`) 
+                                     VALUES('".implode("','",$array)."')";
+        }
+
+       // echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+    public function del($id){
+        $sql="DELETE FROM $this->table WHERE ";
+
+        if(is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+
+            $sql .= implode(" AND ",$tmp);
+        }else{
+            $sql .= " `id`='$id'";
+        }
+
+        return $this->pdo->exec($sql);
+    }
+    public function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //回傳分級文字
+    public function level($level){
+        return $this->level[$level];
+    }
+
+}
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url){
+    header("location:".$url);
+}
+
+$Movie=new DB('movie');
+$Ord=new DB('ord');
+$Poster=new DB('poster');
+
+?>
